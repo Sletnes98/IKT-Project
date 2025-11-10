11cceee1bb020a50e02ce3aaee13be695c91dd7f
Sletnes98 <sondre.bsletnes@gmail.com>
2025-11-10 15:28:57 +0100

    Ny fix og endring av minene
    Hatt mye feilmeldinger som plutselig har kommet opp, plus jeg har endret minene så det er de med lang kjetting

--- Spill_IKT_Projekt/Assets/Scripts/MineSpawnerScript.cs
+++ Spill_IKT_Projekt/Assets/Scripts/MineSpawnerScript.cs
@@ -2,39 +2,38 @@ using UnityEngine;
 
 public class MineSpawnerScript : MonoBehaviour
 {
-
+    [Header("Mine Settings")]
     public GameObject mine;
-    public float spawnRate = 2;
-    public float timer = 0;
-    public float heightOffset = 10;
+    public float spawnRate = 2f;      // tid mellom spawns
+    private float timer = 0f;
 
+    [Header("Spawn Area")]
+    public float minHeight = -2f;     // nederste høyde for spawn
+    public float maxHeight = 2f;      // høyeste høyde for spawn
+    public float xOffset = 12f;       // hvor langt utenfor kamera minene spawner
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         spawnMine();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (timer < spawnRate)
-        {
-            timer = timer + Time.deltaTime;
-        }
-        else
+        timer += Time.deltaTime;
+        if (timer >= spawnRate)
         {
             spawnMine();
-            timer = 0;
+            timer = 0f;
         }
     }
 
     void spawnMine()
     {
-        float lowestPoint = transform.position.y - heightOffset;
-        float highestPoint = transform.position.y + heightOffset;
+        // Bruker faste verdier i stedet for offset fra spawner-posisjonen
+        float randomY = Random.Range(minHeight, maxHeight);
+        float spawnX = transform.position.x + xOffset; // spawner utenfor kamera til høyre
+        Vector3 spawnPos = new Vector3(spawnX, randomY, 0);
 
-         Instantiate(mine, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
+        Instantiate(mine, spawnPos, Quaternion.identity);
     }
-    
 }
