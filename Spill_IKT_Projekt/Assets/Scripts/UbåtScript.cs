0ec5ceafad2f8a118d804561a134a89cc691d682
Sletnes98 <sondre.bsletnes@gmail.com>
2025-11-11 14:23:20 +0100

    Poengsystem, GameOverScreen
    Laget et poengsystem sånn at man får ett poeng av å gå i mellom minene, pluss en Game Over screen med try again knapp slik at man kan starte spillet på nytt

--- Spill_IKT_Projekt/Assets/Scripts/UbåtScript.cs
+++ Spill_IKT_Projekt/Assets/Scripts/UbåtScript.cs
@@ -2,13 +2,20 @@ using UnityEngine;
 
 public class UbåtScript : MonoBehaviour
 {
+    public LogicScript logic;
+    public bool ubåtIsAlive = true;
     public Rigidbody2D myRigidbody;
     public float oppKraft = 7f;
     public float maksOppHastighet = 8f;
 
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+    }
+
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) == true && ubåtIsAlive == true)
         {
             // Legg til kraft oppover så lenge vi ikke går for fort
             if (myRigidbody.linearVelocity.y < maksOppHastighet)
@@ -16,5 +23,10 @@ public class UbåtScript : MonoBehaviour
                 myRigidbody.AddForce(Vector2.up * oppKraft);
             }
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        ubåtIsAlive  = false;
     }
 }
